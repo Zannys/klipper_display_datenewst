e7d99a851fa5e8714ea9bb7d84921a0b6fa40d09
Gabriel Zanetti <zanetti.gabriel@hotmail.com>
2025-11-06 11:11:46 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ echo -e "${YELLOW}Stopping Klipper service.${NC}"
 sudo service klipper stop
 
 echo "Copiando arquivos..."
-cp menu.py ~/klippy/extras/display/menu.py
-cp virtual_sdcard.py ~/klippy/extras/virtual_sdcard.py
+cp menu.py ~/klipper/klippy/extras/display/menu.py
+cp virtual_sdcard.py ~/klipper/klippy/extras/virtual_sdcard.py
 cp sovol-menu.cfg ~/printer_data/config/lcd/sovol-menu.cfg
 
 echo -e "${YELLOW}Starting Klipper service.${NC}"
