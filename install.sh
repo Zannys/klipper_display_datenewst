6f0e9c288ea60a75538cedc70684dbf4077e235a
Gabriel Zanetti <zanetti.gabriel@hotmail.com>
2025-11-06 11:32:15 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 set -e  # Para o script se der erro
 
-# 🧭 Detecta o diretório onde o script está localizado
+#Detecta o diretório onde o script está localizado
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 echo "Stopping Klipper service."
 sudo systemctl stop klipper
 
 echo "Copiando arquivos..."
-cp "$SCRIPT_DIR/menu.py" /home/sovol/klipper/klippy/extras/display/menu.py
-cp "$SCRIPT_DIR/virtual_sdcard.py" /home/sovol/klipper/klippy/extras/virtual_sdcard.py
-cp "$SCRIPT_DIR/sovol-menu.cfg" /home/sovol/klipper_config/display/sovol-menu.cfg
+cp "$SCRIPT_DIR/menu.py" ~/klipper/klippy/extras/display/menu.py
+cp "$SCRIPT_DIR/virtual_sdcard.py" ~/klipper/klippy/extras/virtual_sdcard.py
+cp "$SCRIPT_DIR/sovol-menu.cfg" ~/printer_data/config/sovol-menu.cfg
 
 echo "Starting Klipper service."
 sudo systemctl start klipper
 
-echo "✅ Concluído!"
+echo "Concluído!"
